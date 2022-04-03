e5d160b5a4b043895077cb8d794f779b726e6b26
Asad Kamran <kamranasad7@gmail.com>
2022-04-03 16:44:55 +0500

    Sass installed with custom bootstrap primary color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Router, Route, Routes } from "react-router-dom";
 import React, {useState} from 'react';
 import SideNav from './components/Navbar';
