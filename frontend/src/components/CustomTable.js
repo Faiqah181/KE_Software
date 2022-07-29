e95e0b715379ee0c3111ad0da1c7ebdc9154d8db
Asad Kamran <kamranasad7@gmail.com>
2022-07-29 12:24:12 +0500

    CustomTable hover/click and empty data styling

--- frontend/src/components/CustomTable.js
+++ frontend/src/components/CustomTable.js
@@ -1,17 +1,20 @@
 import { Table } from "react-super-responsive-table";
 import { Input } from "reactstrap";
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import "../css/CustomTable.css"
 
 const CustomTable = (props) => {
     return (
         <div className="custom-table-border">
             {props.searchable && <Input placeholder="Search" onChange={props.searchEvent} />}
+            
             <Table className="custom-table">
                 {props.children}
             </Table>
+            
+            {props.isEmpty && <div className="custom-table-nodata">No Data Available</div>}
         </div>
     )
-    
 }
 
 export default CustomTable;
