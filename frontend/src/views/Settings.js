e9944672f9524d917d23c9a396a7376379fbf116
Faiqah Shuaib <faiqahshuaib123@gmail.com>
2022-09-05 21:34:47 +0500

    Settings page ui

--- frontend/src/views/Settings.js
+++ frontend/src/views/Settings.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { Card, CardTitle, CardSubtitle, CardBody, Form, FormGroup, Label, Input, Button, Row, Col } from 'reactstrap'
 import axios from 'axios';
 import config from "../config";
+import {IconBase, RiLockPasswordFill} from "react-icons"
 import useAuthentication from '../components/useAuthentication';
+import '../css/Settings.css';
 
 const Setting = () => {
 
@@ -16,14 +18,14 @@ const Setting = () => {
 
     const NewPassword = async () => {
         try {
-            if(JSON.stringify(confirmNewPassword)===JSON.stringify(newPassword)){
+            if (JSON.stringify(confirmNewPassword) === JSON.stringify(newPassword)) {
                 await axios.post(`${config.API_URL}/user-credential/${username}`, {
                     headers: { 'x-access-token': user, },
-                    currentPassword : currentPassword,
+                    currentPassword: currentPassword,
                     newPassword: newPassword
                 })
             }
-            
+
         }
         catch (e) {
             console.log(e);
@@ -50,84 +52,99 @@ const Setting = () => {
             <Card>
                 <CardBody>
                     <h1>Settings</h1>
-                    <br/>
-                    <br/>
+                    <br />
                     <h5>Change Password</h5>
                     <p>To change your current password, you must enter your current password to authenticate the user.</p>
                     <div>
                         <div>
                             <Card>
                                 <CardBody>
-                                    <Form inline>
-                                        <FormGroup floating>
-                                            <Input
-                                                id="username"
-                                                name="username"
-                                                placeholder="Enter Username"
-                                                type="text"
-                                                onChange={e => setUserName(e.target.value)}
-                                            />
-                                            <Label for="username">
-                                                Username
-                                            </Label>
-                                        </FormGroup>
-                                        <FormGroup floating>
-                                            <Input
-                                                id="currentPassword"
-                                                name="currentPassword"
-                                                placeholder="Enter Current Password"
-                                                type="password"
-                                                onChange={e => setCurrentPassword(e.target.value)}
-                                            />
-                                            <Label for="password">
-                                                Current Password
-                                            </Label>
-                                        </FormGroup>
-                                        <FormGroup floating>
-                                            <Input
-                                                id="newPassword"
-                                                name="newPassword"
-                                                placeholder="Enter new Password"
-                                                type="password"
-                                                onChange={e => setNewPassword(e.target.value)}
-                                            />
-                                            <Label for="password">
-                                                New Password
-                                            </Label>
-                                        </FormGroup>
-                                        <FormGroup floating>
-                                            <Input
-                                                id="newPasswordConfirm"
-                                                name="newPasswordConfirm"
-                                                placeholder="Re-enter your new Password to Confirm"
-                                                type="password"
-                                                onChange={e => setConfirmedPassword(e.target.value)}
-                                            />
-                                            <Label for="password">
-                                                Confirm New Password
-                                            </Label>
-                                        </FormGroup>
+                                    <Form>
+                                        <Row>
+                                            <Col>
+                                                <FormGroup floating>
+                                                    <Input
+                                                        id="username"
+                                                        name="username"
+                                                        placeholder="Enter Username"
+                                                        type="text"
+                                                        onChange={e => setUserName(e.target.value)}
+                                                    />
+                                                    <Label for="username">
+                                                        Username
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                            <Col>
+                                                <FormGroup floating>
+                                                    <Input
+                                                        id="currentPassword"
+                                                        name="currentPassword"
+                                                        placeholder="Enter Current Password"
+                                                        type="password"
+                                                        onChange={e => setCurrentPassword(e.target.value)}
+                                                    />
+                                                    <Label for="password">
+                                                        Current Password
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <FormGroup floating>
+                                                    <Input
+                                                        id="newPassword"
+                                                        name="newPassword"
+                                                        placeholder="Enter new Password"
+                                                        type="password"
+                                                        onChange={e => setNewPassword(e.target.value)}
+                                                    />
+                                                    <Label for="password">
+                                                        New Password
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                            <Col>
+                                                <FormGroup floating>
+                                                    <Input
+                                                        id="newPasswordConfirm"
+                                                        name="newPasswordConfirm"
+                                                        placeholder="Re-enter your new Password to Confirm"
+                                                        type="password"
+                                                        onChange={e => setConfirmedPassword(e.target.value)}
+                                                    />
+                                                    <Label for="password">
+                                                        Confirm New Password
+                                                    </Label>
+                                                </FormGroup>
+                                            </Col>
+                                        </Row>
                                         <Button color='primary' onClick={NewPassword}>
                                             Change Password
                                         </Button>
-
                                     </Form>
                                 </CardBody>
                             </Card>
                         </div>
                     </div>
-                    <br/>
-                    <br/>
+                    <br />
+                    <br />
                     <h5>BackUp and Restore</h5>
                     <p>Make sure to back up your data atleast every week so that if you system corrupts your data can be restored.</p>
-                    <Label>Last back-up done: </Label>
-                    <p>{lastBackupDate}</p>
-                    <Row>
-                        <Col>
-                            <Button color='primary' onClick={backupData}>Back Up</Button>
-                        </Col>
-                    </Row>
 
+                    <Card>
+                        <CardBody>
+                        <Label>Last back-up done: 14-09-2022 4:00pm</Label>
+                            <p>{lastBackupDate}</p>
+                            <Row>
+                                <Col>
+                                    <Button color='primary' onClick={backupData}>Back Up</Button>
+                                    <Button className='restoreBtn' color='secondary'>Restore</Button>
+                                </Col>
+                            </Row>
+                        </CardBody>
+                    </Card>
                 </CardBody>
             </Card>
 
